ad61e18cd3005fa2dd8a41233e60321275435722
lucrae <lucienraegentil@gmail.com>
2018-11-19 13:48:09 +1100

    Implement user registration and login

--- edulists/__init__.py
+++ edulists/__init__.py
@@ -2,15 +2,19 @@ from flask import Flask
 from flask.cli import FlaskGroup
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from edulists.config import Config
+from flask_login import LoginManager
 
-# blueprint groups
-from edulists.main.routes import main
-from edulists.admin.routes import admin
+from edulists.config import Config
 
 # define components
 db = SQLAlchemy()
 migrate = Migrate()
+login = LoginManager()
+login.login_view = 'main.login'
+
+# import blueprint groups
+from edulists.main.routes import main
+from edulists.admin.routes import admin
 
 def create_app(config_class=Config):
 
@@ -21,6 +25,7 @@ def create_app(config_class=Config):
     # init components
     db.init_app(app)
     migrate.init_app(app, db)
+    login.init_app(app)
 
     # register blueprints
     app.register_blueprint(main, url_prefix='/')
